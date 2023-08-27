066b385e26c35776d1a12b531a59c1b28459dc82
Roy Jacobson <roi.jacobson1@gmail.com>
2023-08-27 15:14:50 +0300

    ring_buffer: Add tests coverage and clean-up the interface a bit.

--- base/ring_buffer.h
+++ base/ring_buffer.h
@@ -3,69 +3,70 @@
 //
 #pragma once
 
+#include <cassert>
 #include <climits>
 #include <memory>
+#include <utility>
 
 namespace base {
 
 // Simple, single-threaded ring buffer.
+// T must be default constructible.
 template <typename T> class RingBuffer {
  public:
   using value_type = T;
 
-  // cap must be power of 2.
-  RingBuffer(unsigned cap) : capacity_(cap) {
+  explicit RingBuffer(unsigned cap) : capacity_(cap) {
     buf_.reset(new T[cap]);
     mask_ = cap - 1;
+    // cap must be power of 2.
+    assert((mask_ & capacity_) == 0);
   }
 
   unsigned size() const {
     return tail_ - head_;
   }
 
-  bool empty() const {
+  [[nodiscard]] bool empty() const {
     return tail_ == head_;
+  }
+
+  [[nodiscard]] bool Full() const {
+    return tail_ - head_ == capacity_;
   }
 
   // Try to inserts into tail of the buffer.
   template <typename U> bool TryEmplace(U&& u) {
-    // due to how 2s compliment work, this check works even in case of overflows.
-    // for example,
-    constexpr unsigned kHead = UINT_MAX;
-    constexpr unsigned kTail = kHead + 10;
-    static_assert(kTail == 9);
-    static_assert(kTail - kHead == 10);
-
-    if (tail_ - head_ < capacity_) {
-      buf_[tail_ & mask_] = std::forward<U>(u);
-      ++tail_;
-      return true;
+    T* slot = GetTail();
+    if (slot) {
+      *slot = std::forward<U>(u);
     }
-    return false;
+    return bool(slot);
   }
 
   // Inserts into tail. If buffer is full overrides the first inserted item at head
   // and keeps the ring at maximal capacity.
   // Returns true if an item was overriden and false otherwise.
   template <typename U> bool EmplaceOrOverride(U&& u) {
-    buf_[tail_ & mask_] = std::forward<U>(u);
-    ++tail_;
-    if (tail_ <= head_ + capacity_) {
-      return false;
-    }
-
-    ++head_;  // override.
-    return true;
+    bool is_full = Full();
+    *GetTail(/*override=*/true) = std::forward<U>(u);
+    return is_full;
   }
 
-  // Gets the pointer to the next tail entry, or null if buffer is full
-  // advances the tail to the next position.
-  T* GetTail() {
+  // Gets a pointer to the next tail entry, if `override` is set, override existing
+  // entries when needed.
+  // Advances the tail to the next position.
+  [[nodiscard]] T* GetTail(bool override = false) {
     T* res = nullptr;
-    if (tail_ - head_ < capacity_) {
-      res = buf_.get() + (tail_ & mask_);
-      ++tail_;
+    if (Full()) {
+      if (override)
+        head_++;
+      else
+        return res;
     }
+
+    res = buf_.get() + (tail_ & mask_);
+    ++tail_;
     return res;
   }
 
@@ -83,14 +84,19 @@ template <typename T> class RingBuffer {
     return capacity_;
   }
 
-  // Zero-copy deque interface.
-  // Returns pointer to array of length size().
-  T* GetItem(unsigned index) {
-    return buf_.get() + ((head_ + index) & mask_);
+  T& operator[](unsigned index) {
+    assert(index < size());
+    return buf_[(head_ + index) & mask_];
+  }
+
+  const T& operator[](unsigned index) const {
+    assert(index < size());
+    return buf_[(head_ + index) & mask_];
   }
 
   // Requires: n <= size()
   void ConsumeHead(unsigned n) {
+    assert(n <= size());
     head_ += n;
   }
 
