860763c524bf8287cfed3a5d721f9ac1e5803db5
Andrew Dunstall <andydunstall@hotmail.co.uk>
2023-09-10 11:51:52 +0100

    feat(s3): pass ownership of http client to s3 read file

--- util/cloud/s3.cc
+++ util/cloud/s3.cc
@@ -249,7 +249,13 @@ io::Result<io::ReadonlyFile*> S3Bucket::OpenReadFile(string_view path,
   } else {
     full_path = absl::StrCat(bucket_, "/", path);
   }
-  return OpenS3ReadFile(region_, full_path, aws_, http_client_.get(), opts);
+
+  unique_ptr http_client = std::move(http_client_);
+  error_code ec = Connect(http_client->connect_timeout_ms());
+  if (ec)
+    return make_unexpected(ec);
+
+  return OpenS3ReadFile(region_, full_path, aws_, http_client.release(), opts);
 }
 
 io::Result<io::WriteFile*> S3Bucket::OpenWriteFile(std::string_view path) {
