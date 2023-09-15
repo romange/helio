990136985059193b7d61187cc7e68015773f52a6
Andrew Dunstall <andydunstall@hotmail.co.uk>
2023-09-15 14:24:25 +0100

    feat(aws): add s3 list buckets demo

--- examples/s3v2_demo.cc
+++ examples/s3v2_demo.cc
@@ -1,6 +1,9 @@
 // Copyright 2023, Roman Gershman.  All rights reserved.
 // See LICENSE for licensing terms.
 
+#include <aws/core/auth/AWSCredentialsProvider.h>
+#include <aws/s3/S3Client.h>
+
 #include <memory>
 
 #include "base/init.h"
@@ -28,6 +31,28 @@ int main(int argc, char* argv[]) {
   pp->Run();
 
   util::cloud::aws::Init();
+
+  Aws::S3::S3ClientConfiguration s3_conf{};
+  // Out HTTP client currently only supports HTTP.
+  s3_conf.scheme = Aws::Http::Scheme::HTTP;
+  // HTTP 1.1 is the latest version our HTTP client supports.
+  s3_conf.version = Aws::Http::Version::HTTP_VERSION_1_1;
+
+  // TODO(andydunstall) For now only support the environment provider.
+  std::shared_ptr<Aws::Auth::AWSCredentialsProvider> credentials_provider =
+      Aws::MakeShared<Aws::Auth::EnvironmentAWSCredentialsProvider>("helio");
+  Aws::S3::S3Client s3{credentials_provider, Aws::MakeShared<Aws::S3::S3EndpointProvider>("helio"),
+                       s3_conf};
+
+  Aws::S3::Model::ListBucketsOutcome outcome = s3.ListBuckets();
+  if (outcome.IsSuccess()) {
+    std::cout << "buckets: " << std::endl;
+    for (const Aws::S3::Model::Bucket& bucket : outcome.GetResult().GetBuckets()) {
+      std::cout << bucket.GetName() << std::endl;
+    }
+  } else {
+    LOG(ERROR) << "failed to list buckets: " << outcome.GetError().GetExceptionName();
+  }
 
   util::cloud::aws::Shutdown();
 }
