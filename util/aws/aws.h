a667c377a9d3f910e5dea975969a89e14c106156
Andrew Dunstall <andydunstall@hotmail.co.uk>
2023-09-18 13:46:51 +0100

    feat(aws): build aws sdk

---
+++ util/aws/aws.h
@@ -0,0 +1,16 @@
+// Copyright 2023, Roman Gershman.  All rights reserved.
+// See LICENSE for licensing terms.
+
+#pragma once
+
+namespace util {
+namespace aws {
+
+// Initialises the AWS library. This must be called before using any AWS
+// services.
+void Init();
+
+void Shutdown();
+
+}  // namespace aws
+}  // namespace util
