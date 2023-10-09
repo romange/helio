d6b23a7b9ccd608a7e16c1fa7b57f7b629901681
Andrew Dunstall <andydunstall@hotmail.co.uk>
2023-10-09 07:48:20 +0100

    feat(aws): log credential provider chain

--- util/aws/credentials_provider_chain.cc
+++ util/aws/credentials_provider_chain.cc
@@ -11,15 +11,32 @@ namespace util {
 namespace aws {
 
 CredentialsProviderChain::CredentialsProviderChain() {
-  AddProvider(std::make_shared<Aws::Auth::EnvironmentAWSCredentialsProvider>());
-  AddProvider(std::make_shared<Aws::Auth::ProfileConfigFileAWSCredentialsProvider>());
+  providers_.push_back(std::make_pair(
+      "environment", std::make_shared<Aws::Auth::EnvironmentAWSCredentialsProvider>()));
+  providers_.push_back(
+      std::make_pair("profile config file",
+                     std::make_shared<Aws::Auth::ProfileConfigFileAWSCredentialsProvider>()));
 
   const auto ec2_metadata_disabled = Aws::Environment::GetEnv("AWS_EC2_METADATA_DISABLED");
   if (Aws::Utils::StringUtils::ToLower(ec2_metadata_disabled.c_str()) != "true") {
-    AddProvider(std::make_shared<Aws::Auth::InstanceProfileCredentialsProvider>());
+    providers_.push_back(std::make_pair(
+        "ec2 metadata", std::make_shared<Aws::Auth::InstanceProfileCredentialsProvider>()));
   } else {
     LOG(INFO) << "aws: disabled EC2 metadata";
   }
+}
+
+Aws::Auth::AWSCredentials CredentialsProviderChain::GetAWSCredentials() {
+  for (const auto& provider : providers_) {
+    Aws::Auth::AWSCredentials credentials = provider.second->GetAWSCredentials();
+    if (!credentials.GetAWSAccessKeyId().empty() && !credentials.GetAWSSecretKey().empty()) {
+      LOG_FIRST_N(INFO, 1) << "aws: loaded credentials; provider=" << provider.first;
+      return credentials;
+    }
+  }
+
+  LOG(ERROR) << "aws: failed to load credentials; no credential provider found";
+  return Aws::Auth::AWSCredentials{};
 }
 
 }  // namespace aws
