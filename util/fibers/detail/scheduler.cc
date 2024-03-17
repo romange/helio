e332548ccb27cdefb70913dae2d36b842a9e8317
Roman Gershman <romange@gmail.com>
2024-03-17 11:01:36 +0200

    chore: add retry behavior inside ProcessRemoteReady

--- util/fibers/detail/scheduler.cc
+++ util/fibers/detail/scheduler.cc
@@ -85,9 +85,9 @@ DispatcherImpl::~DispatcherImpl() {
 
 ctx::fiber DispatcherImpl::Run(ctx::fiber&& c) {
 #if defined(BOOST_USE_UCONTEXT)
-    std::move(c).resume();
+  std::move(c).resume();
 #else
-    DCHECK(!c);
+  DCHECK(!c);
 #endif
 
   if (scheduler_->policy()) {
@@ -324,21 +324,29 @@ bool Scheduler::ProcessRemoteReady(FiberInterface* active) {
     if (!fi) {
       if (qempty) {
         if (active && !res) {
-          // UB state.
+          // UB state. Try to recover.
           FiberInterface* next = active->remote_next_.load(std::memory_order_acquire);
           LOG(ERROR) << "Failed to pull active fiber from remote_ready_queue, iteration "
                      << iteration << " remote_empty: " << remote_ready_queue_.Empty()
                      << ", next:" << (uint64_t)next;
-          if (next && next != (FiberInterface*)FiberInterface::kRemoteFree) {
-            LOG(ERROR) << "Next fiber next is: "
-                       << next->remote_next_.load(std::memory_order_acquire) << ", usecount"
-                       << next->use_count_.load(std::memory_order_relaxed);
+
+          if (next != (FiberInterface*)FiberInterface::kRemoteFree) {
+            LOG_IF(ERROR, next != nullptr)
+                << "Next fiber next is: " << next->remote_next_.load(std::memory_order_acquire)
+                << ", usecount: " << next->use_count_.load(std::memory_order_relaxed);
+            if (iteration < 100) {
+              // Work around the inconsistency by retrying.
+              ++iteration;
+              continue;
+            }
           }
         }
         break;
+      } else {  // qempty == false, retry.
+        ++iteration;
+        VLOG(1) << "Retrying " << iteration;
+        continue;
       }
-      DVLOG(1) << "Retrying " << iteration++;
-      continue;
     }
 
     // Marks as free.
