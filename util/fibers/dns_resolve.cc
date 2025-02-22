7fc3a33f54bfcb873c1ac32efefc6a9740edf91b
Roman Gershman <romange@gmail.com>
2025-02-22 22:28:18 +0200

    chore: add a process wide DnsResolver singleton (#382)

--- util/fibers/dns_resolve.cc
+++ util/fibers/dns_resolve.cc
@@ -24,6 +24,7 @@ namespace {
 
 struct DnsResolveCallbackArgs {
   char* dest_ip = nullptr;
+  fb2::detail::FiberInterface* cntx = nullptr;
   std::error_code ec;
   bool done = false;  // Should we use optional<ec> above instead of the additional bool field?
 };
@@ -99,7 +100,7 @@ void UpdateSocketsCallback(void* arg, ares_socket_t socket_fd, int readable, int
         };
         socket_state.arm_index = uring->EpollAdd(socket_fd, std::move(cb), mask);
         DVLOG(1) << "EpollAdd " << socket_fd << ", mask: " << mask
-                << " index: " << socket_state.arm_index;
+                 << " index: " << socket_state.arm_index;
 #endif
       }
     } else {
@@ -112,6 +113,10 @@ void DnsResolveCallback(void* ares_arg, int status, int timeouts, struct ares_ad
   auto* cb_args = static_cast<DnsResolveCallbackArgs*>(ares_arg);
   cb_args->done = true;
   VLOG(1) << "DnsResolveCallback: " << status << " " << timeouts << " " << (res ? res->name : "");
+
+  if (cb_args->cntx) {
+    fb2::detail::FiberActive()->ActivateOther(cb_args->cntx);
+  }
 
   if (status != ARES_SUCCESS || res->nodes == nullptr) {
     cb_args->ec = make_error_code(errc::address_not_available);
@@ -141,15 +146,6 @@ void DnsResolveCallback(void* ares_arg, int status, int timeouts, struct ares_ad
   }
 }
 
-// TODO: to redesign the whole logic to make it a process-wide singleton.
-// ProcessChannel should become a handler running by the proactor.
-// Instead of blocking on cv, we can use proactor to set up a timer.
-// ProcessChannel should not be call specific, and should serve multiple calls and should run
-// indefinitely together with the proactor.
-// The way I imagine it should work, ProactorBase should enable DNS functionality.
-// ProactorPool enables DNS functionality by choosing a single proactor from the pool.
-// DnsResolve knows which proactor handles dns requests and makes hops to pass Dns requests
-// to the proactor.
 void ProcessChannel(ares_channel channel, AresChannelState* state, DnsResolveCallbackArgs* args) {
   while (!args->done) {
     // It's important to set and reset fiber_ctx close to Suspend, to avoid the case
@@ -184,7 +180,38 @@ void ProcessChannel(ares_channel channel, AresChannelState* state, DnsResolveCal
   }
 }
 
+ares_channel* g_channel = nullptr;
+
 }  // namespace
+
+void InitDnsResolver(uint32_t timeout_ms) {
+  CHECK(!g_channel) << "Already initialized";
+  constexpr int kOptions = ARES_OPT_LOOKUPS | ARES_OPT_TIMEOUTMS | ARES_OPT_EVENT_THREAD;
+
+  ares_options options = {};
+  char lookups[] = "fb";
+  options.lookups = lookups;  // hosts file first, then DNS
+  options.timeout = timeout_ms;
+
+  g_channel = new ares_channel;
+  CHECK_EQ(ares_init_options(g_channel, &options, kOptions), ARES_SUCCESS);
+}
+
+// TODO: Replace DnsResolve with much simpler DnsResolve2.
+error_code DnsResolve(const string& host, char dest_ip[]) {
+  CHECK(g_channel) << "Must call InitDnsResolver before DnsResolve2";
+  ares_channel* channel = g_channel;
+
+  ares_addrinfo_hints hints{ARES_AI_CANONNAME, AF_UNSPEC, 0, 0};
+
+  DnsResolveCallbackArgs cb_args;
+  cb_args.dest_ip = dest_ip;
+  cb_args.cntx = fb2::detail::FiberActive();
+  ares_getaddrinfo(*channel, host.c_str(), NULL, &hints, DnsResolveCallback, &cb_args);
+  fb2::detail::FiberActive()->Suspend();
+
+  return cb_args.ec;
+}
 
 error_code DnsResolve(const string& host, uint32_t wait_ms, char dest_ip[],
                       ProactorBase* proactor) {
