2c9a37e59db59441627dc52ac2561046a64e1926
Roman Gershman <romange@gmail.com>
2025-09-02 11:36:27 +0300

    chore:include DISPATCH fiber in long running warnings
    Also improve log precision.
    
    Signed-off-by: Roman Gershman <romange@gmail.com>

--- util/fibers/detail/scheduler.cc
+++ util/fibers/detail/scheduler.cc
@@ -163,7 +163,7 @@ Scheduler::Scheduler(FiberInterface* main_cntx) : main_cntx_(main_cntx) {
 
   fibers_.push_back(*main_cntx);
   fibers_.push_back(*dispatch_cntx_);
-  AddReady(dispatch_cntx_.get());
+  ready_queue_[unsigned(FiberPriority::NORMAL)].push_back(*dispatch_cntx_);
 }
 
 Scheduler::~Scheduler() {
@@ -235,7 +235,12 @@ void Scheduler::AddReady(FiberInterface* fibi, bool to_front) {
   DCHECK(!fibi->list_hook.is_linked());
   DVLOG(2) << "Adding " << fibi->name() << " to ready_queue_";
 
-  fibi->cpu_tsc_ = CycleClock::Now();
+  // We measure runqueue time for fibers that are not active. Yielding fibers are
+  // excluded, otherwise they won't be accounted correctly by SwitchSetup when
+  // calculating run_cycles.
+  if (FiberActive() != fibi) {
+    fibi->set_cpu_tsc(CycleClock::Now());
+  }
   if (to_front) {
     ready_queue_[unsigned(FiberPriority::NORMAL)].push_front(*fibi);
   } else {
@@ -502,6 +507,7 @@ void Scheduler::PrintAllFiberStackTraces() {
 auto Scheduler::RunWorkerFibersStepImpl() -> RunFiberResult {
   const unsigned q_indx = unsigned(FiberPriority::NORMAL);
   DCHECK(HasReady(q_indx));
+  DCHECK(FiberActive() == dispatch_cntx_.get());
 
   ProactorBase::UpdateMonotonicTime();
 
